f3da64072eb3492f9da410fa31d4be16966653f1
Pedro Dobrescu <pedro@presslabs.com>
2015-06-01 16:50:59 +0300

    Creating the child theme

---
+++ wp-content/themes/artificer-child/functions.php
@@ -0,0 +1,5 @@
+add_action( 'wp_enqueue_scripts', 'artificer_enqueue_styles' );
+function artificer_enqueue_styles() {
+    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+
+}
